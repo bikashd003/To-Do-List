c3cabda435b3b11db7feb751dfba4855b66f211f
Bikash Das <99291618+bikashd003@users.noreply.github.com>
2022-06-23 22:51:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require ("express");
+const bodyParser = require("body-parser");
+const app =express();
+
+let items=[];
+let workItems=[];
+app.set('view engine',"ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+app.get("/",(req,res)=>{
+    const today= new Date();
+    let options={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+  let day=today.toLocaleDateString("en-US",options);
+    res.render("list",{listTitle:day,newItems:items});
+});
+
+app.post("/",(req,res)=>{
+    let item=req.body.newItem;
+    if(req.body.list === "Work List")
+    {
+          workItems.push(item);
+          res.redirect("/work");
+    }
+    else
+    {
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work",(req,res)=>{
+    res.render("list",{listTitle:"Work List",newItems:workItems})
+});
+// app.post("/work",(req,res)=>{
+//   let item=req.body.newItem;
+//   workItems.push(item);
+//   res.redirect("/work");
+// })
+
+
+
+app.listen(3000,()=>{
+    console.log("servar is started on port 3000");
+})
